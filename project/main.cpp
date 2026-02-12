f083e3af89edc11c67df8ce86319a9ee05f7c054
sato-yui-13 <k024g0002@g.neec.ac.jp>
2026-02-12 14:08:57 +0900

    5

--- project/main.cpp
+++ project/main.cpp
@@ -164,13 +164,6 @@ static LONG WINAPI ExportDump(EXCEPTION_POINTERS* exception) {
 
 #pragma endregion
 
-Vector3 Normalize(const Vector3& v) {
-	float length = std::sqrt(v.x * v.x + v.y * v.y + v.z * v.z);
-	if (length == 0.0f)
-		return { 0.0f, 0.0f, 0.0f };
-	return { v.x / length, v.y / length, v.z / length };
-}
-
 
 
 #pragma region objファイルを読み込む関数
